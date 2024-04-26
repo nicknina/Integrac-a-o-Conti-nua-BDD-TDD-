99f1c3810cd93abd25411234db4714b0b7b7698c
Nickão <80963275+nicknina@users.noreply.github.com>
2024-04-25 23:55:14 -0300

    Create MediaDe2.py

---
+++ MediaDe2.py
@@ -0,0 +1,5 @@
+def MediaDe2(num1, num2):
+    if not (isinstance(num1, (int, float)) and isinstance(num2, (int, float))):
+        raise TypeError("Os números devem ser inteiros ou floats")
+    
+    return (num1 + num2) / 2
